987f3280c77aee006d8ab5d83d01e559cfb452df
Nathan Lehman <njlehman1@gmail.com>
2015-02-02 14:29:10 -0600

    fixed compilation errors

--- Deck.cpp
+++ Deck.cpp
@@ -1,7 +1,8 @@
 #include "Deck.h"
 #include <time.h>
+#include <cstdlib>
 
-Deck::Deck():size(0) {
+Deck::Deck(){
   srand((size_t) time(NULL));
 }
 
@@ -15,46 +16,20 @@ void Deck::addCard(Card newCard) {
   cardStack.push(newCard);
 }
 
-/**
-I know this is a really bad shuffle function if you wanted completely random
-shuffling. But its also closer to how people really shuffle cards. Honestly I'm
-torn at the moment.
-
 void Deck::shuffle() {
-  if (this->numCards() >= 2) {
-    Deck shuf1;
-    Deck shuf2;
-    while (this->numCards() >= 2) {
-      shuf1.addCard(this->drawCard());
-      shuf2.addCard(this->drawCard());
-    }
-    shuf1.addCard(this->drawCard());
-    while (!shuf1.isEmpty() || !shuf2.isEmpty()) {
-      if (shuf1.isEmpty()) {
-	this->addCard(shuf2.drawCard());
-      } else if (shuf2.isEmpty()) {
-	this->addCard(shuf1.drawCard());
-      } else {
-	(rand()/RAND_MAX) ?
-	  (this->addCard(shuf1.drawCard())) : (this->addCard(shuf2.drawCard()));
-      }
-    }
-  }
-}
-**/
-
-void Deck::shuffle() {
-  if ((size_t deckSize = this->numCards()) >= 2) {
-    Card DeckArr[deckSize];
+  size_t deckSize = this->numCards();
+  if (deckSize >= 2) {
+    Card *deckArr = new Card[deckSize];
     for (size_t i = 0; i < deckSize; i++) {
       size_t j = rand() % (i + 1);
       if (j != i) {
-	DeckArr[i] = DeckArr[j];
+	deckArr[i] = deckArr[j];
       }
-      DeckArr[j] = this-drawCard();
+      deckArr[j] = this->drawCard();
     }
     for (size_t i = 0; i < deckSize; i++) {
-      this->addCard(DeckArr[i]);
+      this->addCard(deckArr[i]);
     }
+    delete [] deckArr;
   }
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-pazaak:main.cpp Deck.h Deck.cpp Card.cpp Card.h Stack_t.cpp Stack_t.h
+pazaak:main.cpp Deck.h Deck.cpp Card.cpp Card.h
 	g++ -Wall main.cpp Deck.cpp Card.cpp -o pazaak
